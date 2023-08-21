0f0a000ed562b107e6710892d3024ee00f6f047b
Humberto <hroliveira@google.com>
2023-08-21 10:39:25 +0100

    small improvements in the setup msgs as we add support for multi account MCAs
    Change-Id: Ia4b4f1dabdbeb5072a71d4c38b52b29b12e97d15

--- setup-wfs.sh
+++ setup-wfs.sh
@@ -15,27 +15,37 @@
 # limitations under the License.
 
 until [[ "$yn" == [YyNn] ]]; do
-    read -p "Would you like to connect your Merchant Center accounts for product performance insights? (y/n): " yn
+    msg='Would you like to connect your Merchant Center accounts '
+    msg+='for product performance insights? (y/n): '
+    read -p "$msg" yn
 done
 
-if [ "$yn" == "y" ]; then
+if [[ "$yn" == "y"  || "$yn" == "Y" ]]; then
     until [[ "$mcid" == "skip" ]] || [[ "$mcid" =~ ^[0-9]+$ ]]; do
-        read -p "Please provide your Merchant Center ID (e.g. 123456 or type 'skip' to skip this step): " mcid
+        msg='Please provide your Merchant Center or MCA ID '
+        msg+='(e.g. 123456 or type 'skip' to skip this step): '
+        read -p "$msg" mcid
     done
 
     if [[ "$mcid" =~ ^[0-9]+$ ]]; then
         
-        echo "creating new merchant_center_transfer dataset in BigQuery..."
+        echo "creating a dataset in BigQuery named merchant_center_transfer..."
         # TODO: Make data_location dynamic:
         bq mk -d --data_location=EU merchant_center_transfer
 
-        echo "creating new data transfer for merchant_center in BigQuery..."
+        echo "creating a Data Transfer for Merchant Center in BigQuery..."
+        bq_mk_params='{"merchant_id":"'$mcid'",'
+        bq_mk_params=+='"export_products":"true",'
+        bq_mk_params=+='"export_regional_inventories":"true",'
+        bq_mk_params=+='"export_local_inventories":"true",'
+        bq_mk_params=+='"export_price_benchmarks":"true",'
+        bq_mk_params=+='"export_best_sellers":"true"}'
         bq mk --transfer_config \
             --project_id="$DEVSHELL_PROJECT_ID" \
             --data_source=merchant_center \
             --display_name=merchant_center_transfer \
             --target_dataset=merchant_center_transfer \
-            --params='{"merchant_id":"'$mcid'","export_products":"true","export_regional_inventories":"true","export_local_inventories":"true","export_price_benchmarks":"true","export_best_sellers":"true"}'
+            --params=$bq_mk_params
     else
         echo "skipping GMC setup..."
     fi
@@ -44,5 +54,5 @@ else
     echo "skipping GMC setup..."
 fi
 
-echo "installing Google Ads data ETL Workflow..."
+echo "initializing Google Ads data ETL Workflow..."
 npm init gaarf-wf@latest -- --answers=answers.json
