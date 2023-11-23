38a8d770c15f1b893bf3feb4c0b6edb1afeb0ef2
Igor Mosin <imosin@google.com>
2023-11-23 16:52:11 +0400

    Asset Group Best Practices Update
    Change-Id: I212be1c10d1cb9b206e758a9ba430eac5408bae6

--- bq_queries/11-assetgroupbestpracticessnapshots.sql
+++ bq_queries/11-assetgroupbestpracticessnapshots.sql
@@ -23,7 +23,8 @@ WITH video_data AS (
     asset_group_name,
     ad_strength,
     is_video_uploaded,
-    CAST(1 as FLOAT64) AS video_score
+    count_videos,
+    IF(count_videos < 3, CAST(0 AS FLOAT64), CAST(1 as FLOAT64)) AS video_score
   FROM `{bq_dataset}_bq.video_assets`
 ),
 text_data AS (
@@ -38,7 +39,7 @@ text_data AS (
     count_long_headlines,
     count_short_descriptions,
     count_short_headlines,
-    (IF(count_descriptions >= 5,1,0)+IF(count_headlines >= 5,1,0)+IF(count_long_headlines >= 1,1,0)+IF(count_short_descriptions >= 1,1,0)+IF(count_short_headlines >= 1,1,0))/5 AS text_score
+    (IF(count_descriptions >= 5,1,0)+IF(count_headlines >= 15,1,0)+IF(count_long_headlines >= 1,1,0)+IF(count_short_descriptions >= 1,1,0)+IF(count_short_headlines >= 1,1,0))/5 AS text_score
   FROM `{bq_dataset}_bq.text_assets`
 ),
 image_data AS (
@@ -51,37 +52,56 @@ image_data AS (
     count_images,
     count_logos,
     count_landscape,
+    count_portrait,
     count_landscape_logos,
     count_square,
     count_square_logos,
-    (IF(count_images >= 20,1,0)+IF(count_logos >= 5,1,0)+IF(count_landscape >= 1,1,0)+IF(count_landscape_logos >= 1,1,0)+IF(count_square >= 1,1,0)+IF(count_square_logos >= 1,1,0))/6 AS image_score
+    (IF(count_landscape >= 5,1,0)+IF(count_square >= 5,1,0)+IF(count_portrait >= 5,1,0)+IF(count_landscape_logos >= 1,1,0)+IF(count_square_logos >= 1,1,0))/5 AS image_score
   FROM `{bq_dataset}_bq.image_assets`
-)
+),
+audience_signals_count AS (
+    SELECT 
+      COUNT(DISTINCT AGS.asset_group_id) AS number_of_audience_signals, 
+      AGS.campaign_id AS campaign_id,
+      AGS.asset_group_id
+    FROM `{bq_dataset}.assetgroupsignal` AGS
+    GROUP BY 
+      campaign_id,
+      asset_group_id
+  )
 SELECT
     CURRENT_DATE() AS date,
     AGS.account_id,
     AGS.account_name,
     AGS.campaign_id,
-    AGS.campaign_name,
+    AGS.campaign_name,  
     AGS.asset_group_id,
     AGS.asset_group_name,
     AGS.ad_strength,
     V.is_video_uploaded,
-    IF(T.count_descriptions >= 5,"Yes","X") AS count_descriptions,
-    IF(T.count_headlines >= 5,"Yes","X") AS count_headlines,
-    IF(T.count_long_headlines >= 1,"Yes","X") AS count_long_headlines,
-    IF(T.count_short_descriptions >= 1,"Yes","X") AS count_short_descriptions,
-    IF(T.count_short_headlines >= 1,"Yes","X") AS count_short_headlines,
-    IF(I.count_images >= 15,"Yes","X") AS count_images,
-    IF(I.count_logos >= 5,"Yes","X") AS count_logos,
-    IF(I.count_landscape >= 1,"Yes","X") AS count_landscape,
-    IF(I.count_landscape_logos >= 1,"Yes","X") AS count_landscape_logos,
-    IF(I.count_square >= 1,"Yes","X") AS count_square,
-    IF(I.count_square_logos >= 1,"Yes","X") AS count_square_logos,
     V.video_score,
     T.text_score,
-    I.image_score
+    I.image_score,
+    IF(count_descriptions < 5, 5 - count_descriptions, 0) AS missing_descriptions,
+    IF(count_headlines < 15, 15 - count_headlines, 0) AS missing_headlines,
+    IF(count_long_headlines < 1, 1 - count_long_headlines, 0) AS missing_long_headlines,
+    IF(count_short_headlines < 1, 1 - count_short_headlines, 0) AS missing_short_headlines,
+    IF(count_short_descriptions < 1, 1 - count_short_descriptions, 0) AS missing_short_descriptions,
+    IF(count_descriptions - count_short_descriptions < 4, 4 - (count_descriptions - count_short_descriptions), 0) AS missing_long_descriptions,
+    IF((count_landscape + count_square + count_portrait) < 20, 20 - (count_landscape + count_square + count_portrait), 0) AS missing_images,
+    IF(count_landscape < 5, 5 - count_landscape, 0) AS missing_landscape,
+    IF(count_square < 5, 5 - count_square, 0) AS missing_square,
+    IF(count_portrait < 5, 5 - count_portrait, 0) AS missing_portrait,
+    IF(count_logos < 2, 2 - count_logos, 0) AS missing_logos,
+    IF(count_square_logos = 0, 1, 0) AS missing_square_logos,
+    IF(count_landscape_logos = 0, 1, 0) AS missing_landscape_logos,
+    IF(count_videos < 3, 3 - count_videos, 0) AS missing_video,
+    IF(ASA.number_of_audience_signals IS NOT NULL,"Yes","X") AS audience_signals
 FROM `{bq_dataset}.assetgroupsummary` AGS
-JOIN video_data V USING (account_id,campaign_id,asset_group_id)
-JOIN text_data T USING (account_id,campaign_id,asset_group_id)
-JOIN image_data I USING (account_id,campaign_id,asset_group_id)
+JOIN video_data V USING (account_id, campaign_id, asset_group_id)
+JOIN text_data T USING (account_id,campaign_id, asset_group_id)
+JOIN image_data I USING (account_id,campaign_id, asset_group_id)
+LEFT JOIN audience_signals_count AS ASA
+ ON AGS.campaign_id = ASA.campaign_id
+ AND AGS.asset_group_id = ASA.asset_group_id
+WHERE AGS.campaign_id IN (SELECT campaign_id FROM `{bq_dataset}.campaign_settings`)
