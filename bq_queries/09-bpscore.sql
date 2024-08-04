73b81340a6993ddada292e0f4f4698d1fad47621
Sergei Dorogin <segy@google.com>
2024-08-04 16:26:52 +0300

    bq-queries: Optimized bpscore.sql query (16-20 minutes => 1 second) scripts: shell scripts have made executable (added +x flag)
    Change-Id: I0a38876ee579b509037ac8e48263ccf90c659336

--- bq_queries/09-bpscore.sql
+++ bq_queries/09-bpscore.sql
@@ -14,29 +14,25 @@
 
 CREATE OR REPLACE TABLE `{bq_dataset}_bq.campaignbpscore_${format(today(),'yyyyMMdd')}` AS
 ( 
-  WITH location_targeting AS (
-  SELECT
-    campaign_id,
-    negative_geo_target_type_configured_good,
-    positive_geo_target_type_configured_good,
-    CASE 
-      WHEN positive_geo_target_type_configured_good='X' AND negative_geo_target_type_configured_good='X' THEN 0
-      WHEN positive_geo_target_type_configured_good='Yes' AND negative_geo_target_type_configured_good='X' THEN 0.5
-      WHEN positive_geo_target_type_configured_good='X' AND negative_geo_target_type_configured_good='Yes' THEN 0.5
-      WHEN positive_geo_target_type_configured_good='Yes' AND negative_geo_target_type_configured_good='Yes' THEN 1
-    END AS location_targeting_score
-  FROM
-    `{bq_dataset}_bq.campaign_data`
-  )   
   SELECT
     date,
     CD.account_id,
     CD.account_name,
     CD.campaign_id,
     CD.campaign_name,
-    round((if(CD.url_expansion_opt_out=true,1,0)+CD.audience_signals_score+if(CD.missing_sitelinks=0,1,missing_sitelinks/4)+LT.location_targeting_score)/4,2) AS campaign_bp_score
+    round(
+      (
+        IF(CD.url_expansion_opt_out=true,1,0) +
+        CD.audience_signals_score +
+        IF(CD.missing_sitelinks=0,1,missing_sitelinks/4) + 
+        CASE 
+          WHEN positive_geo_target_type_configured_good='X' AND negative_geo_target_type_configured_good='X' THEN 0
+          WHEN positive_geo_target_type_configured_good='Yes' AND negative_geo_target_type_configured_good='X' THEN 0.5
+          WHEN positive_geo_target_type_configured_good='X' AND negative_geo_target_type_configured_good='Yes' THEN 0.5
+          WHEN positive_geo_target_type_configured_good='Yes' AND negative_geo_target_type_configured_good='Yes' THEN 1
+        END
+      )/4,2
+    ) AS campaign_bp_score
   FROM `{bq_dataset}_bq.campaign_data`AS CD 
-  LEFT JOIN location_targeting AS LT
-    ON CD.campaign_id = LT.campaign_id
   GROUP BY 1,2,3,4,5,6
 )
